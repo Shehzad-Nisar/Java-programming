e97dc705d616f1fcda83794a343dfbb9fc9e8dbf
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-08-17 19:46:31 +0500

    java collections:

---
+++ Collections/src/List/BankAcc.java
@@ -0,0 +1,10 @@
+package List;
+
+import java.util.ArrayList;
+
+class BankAcc{
+    private double balance;
+    private ArrayList<Transaction> transactions;
+
+
+}
---
+++ Collections/src/List/Transaction.java
@@ -0,0 +1,22 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Transaction {
+    private String type;
+    private double amount;
+
+
+    public Transaction(String type, double amount){
+        this.type = type ;
+        this.amount = amount;
+    }
+
+    public String getType(){
+        return type;
+    }public double getAmount(){
+        return amount;
+    }
+}
+
