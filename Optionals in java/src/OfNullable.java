58ad10f7fddf4c3bbf1b59672cc9ec9e6f079e02
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-08-24 22:01:23 +0500

    optionals

--- Optionals in java/src/OfNullable.java
+++ Optionals in java/src/OfNullable.java
@@ -13,6 +13,17 @@ public class OfNullable {
     }
 
     static void main() {
+
+        Optional<String> optionalS = Optional.of("kazim");
+        Optional<String> optionalS2 = Optional.empty();
+        Optional<String> optionalS3 = Optional.ofNullable(null);
+        System.out.println(optionalS);
+        System.out.println(optionalS2);
+        System.out.println(optionalS3);
+
+
+        System.out.println("Calling method which may or mabe return null.");
         System.out.println(getSecondaryNum(1051));
+        System.out.println(getSecondaryNum(101));
     }
 }
