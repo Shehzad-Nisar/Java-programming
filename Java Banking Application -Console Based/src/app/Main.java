726f8c569242e5e8bc36e44958f03d9ea340681a
Shehzad-Nisar <shehzadnisar07@gmail.com>
2026-08-25 21:51:42 +0500

    improved logics in main class

--- Java Banking Application -Console Based/src/app/Main.java
+++ Java Banking Application -Console Based/src/app/Main.java
@@ -25,13 +25,46 @@ class Main{
             System.out.println("CHOICE: " + choice);
 
             switch (choice){
-                case 0:
+                case 1 -> openAccount(scanner);
+                case 2 -> deposit(scanner);
+                case 3 -> withdraw(scanner);
+                case 4 -> transfer(scanner);
+                case 5 -> accountStatement(scanner);
+                case 6 -> listOfAccounts(scanner);
+                case 7 -> searchAccountByName(scanner);
+                case 8 -> {
                     running = false;
-                    System.out.println("THANK YOU!");
-                    break;
+                    System.out.println("Thank you");
+                }
+                default -> {
+                    running = false ;
+                    System.out.println("Please enter a valid option (1-8).");
+                }
+
             }
         }
 
+    }
+
+    private static void openAccount(Scanner scanner) {
+    }
+
+    private static void deposit(Scanner scanner) {
+    }
+
+    private static void withdraw(Scanner scanner) {
+    }
+
+    private static void transfer(Scanner scanner) {
+    }
+
+    private static void accountStatement(Scanner scanner) {
+    }
+
+    private static void listOfAccounts(Scanner scanner) {
+    }
+
+    private static void searchAccountByName(Scanner scanner) {
     }
 
 }
